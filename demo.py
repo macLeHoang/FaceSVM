a37666971f52c94d2bdc0b5569c1fd29b9737e40
Le Minh Hoang <lehoang.20092000@gmail.com>
2024-12-03 21:27:46 +0700

    update face detector

--- demo.py
+++ demo.py
@@ -2,22 +2,11 @@ import torch
 import numpy as np
 from numpy.linalg import norm
 import cv2
-from deepface.DeepFace import extract_faces
 from models import resnet_face18
 import os
 import glob
 
 
-def get_face(im):
-    result = extract_faces(im, detector_backend="retinaface")
-    facial_area = result[0]["facial_area"]
-    x = facial_area['x']
-    y = facial_area['y']
-    w = facial_area['w']
-    h = facial_area['h']
-    face = im[y:y+h, x:x+w]
-
-    return face
 
 def preprocess(image):
     image = cv2.resize(image, (128, 128), cv2.INTER_AREA)
@@ -50,45 +39,73 @@ if __name__ == "__main__":
     model.load_state_dict(state_dict, strict=True)
     model.to(device)
     model.eval()
+
+    prototxtPath = r"face_detector/deploy.prototxt"
+    weightsPath = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+    faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
     
-    face1 = get_face(face1)
-    cv2.imwrite(os.path.join(save_demo, "query.jpg"), face1)
-    pre1 = preprocess(face1)
-    ims = torch.from_numpy(pre1).to(device)
-    with torch.no_grad():
-        feat = model(ims)
-    feat = feat.cpu().numpy() # query im feature
-
-    database = "database"
-    names = os.listdir(database)
-
-    most_sim = None
-    most_sim_conf = -1
-    idx = None
-
-    for name in names:
-        dfeat = np.load(os.path.join(database, name, "feat.npy"))
-        cosine = (feat @ dfeat.T) / (norm(feat, axis=-1) * norm(dfeat, axis=-1))
-
-        indexes = np.argsort(cosine)[0, -5:] # get maxmimum most k similar images
-        cosine = cosine[0, indexes]
-        print(cosine, cosine.mean(), indexes)
-
-        conf = (cosine.mean() + 1) / 2
-        # conf = np.arccos(cosine.mean()) / np.pi
-
-        if conf > 0.8 and conf > most_sim_conf:
-            most_sim = name
-            most_sim_conf = conf
-            idx = indexes
     
-    print(most_sim, most_sim_conf)
-    if most_sim is not None:
-        cv2.putText(face, f"{most_sim} - {most_sim_conf}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+    (h, w) = face1.shape[:2]
+    blob = cv2.dnn.blobFromImage(face1, 1.0, (224, 224),
+        (104.0, 177.0, 123.0))
+    faceNet.setInput(blob) # pass the blob through the network and obtain the face detections
+    detections = faceNet.forward()
+    locs = []
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            locs.append((startX, startY, endX, endY))
+    
+    if not len(locs):
+        cv2.putText(face, f"No Face in image", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
             1, (0,255,0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(face, f"Stranger", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-            1, (0,255,0), 2, cv2.LINE_AA)
+        locs_max = max(locs, key=lambda x: (x[2]-x[0]) * ([x[3]-x[1]]))
+        (startX, startY, endX, endY) = locs_max
+        face1 = face1[startY:endY, startX:endX]
+
+        cv2.imwrite(os.path.join(save_demo, "query.jpg"), face1)
+        pre1 = preprocess(face1)
+        ims = torch.from_numpy(pre1).to(device)
+        with torch.no_grad():
+            feat = model(ims)
+        feat = feat.cpu().numpy() # query im feature
+
+        database = "database"
+        names = os.listdir(database)
+
+        most_sim = None
+        most_sim_conf = -1
+        idx = None
+
+        for name in names:
+            dfeat = np.load(os.path.join(database, name, "feat.npy"))
+            cosine = (feat @ dfeat.T) / (norm(feat, axis=-1) * norm(dfeat, axis=-1))
+
+            indexes = np.argsort(cosine)[0, -5:] # get maxmimum most k similar images
+            cosine = cosine[0, indexes]
+            print(cosine, cosine.mean(), indexes)
+
+            conf = (cosine.mean() + 1) / 2
+            # conf = np.arccos(cosine.mean()) / np.pi
+
+            if conf > 0.8 and conf > most_sim_conf:
+                most_sim = name
+                most_sim_conf = conf
+                idx = indexes
+        
+        print(most_sim, most_sim_conf)
+        if most_sim is not None:
+            cv2.putText(face, f"{most_sim} - {most_sim_conf}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                1, (0,255,0), 2, cv2.LINE_AA)
+        else:
+            cv2.putText(face, f"Stranger", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                1, (0,255,0), 2, cv2.LINE_AA)
     
     cv2.imshow("face", face)
     cv2.waitKey(0)
--- demo_webcam.py
+++ demo_webcam.py
@@ -2,14 +2,21 @@ import torch
 import numpy as np
 from numpy.linalg import norm
 import cv2
-from deepface.DeepFace import extract_faces
+# from deepface.DeepFace import extract_faces
 from models import resnet_face18
 import os
 import glob
 
 
 def get_face(im):
-    result = extract_faces(im, detector_backend="retinaface")
+    try:
+        result = extract_faces(im, detector_backend="retinaface")
+    except:
+        return None
+    
+    # sort faces by area
+    result = list(sorted(result, key=lambda x: x["facial_area"]['w']*x["facial_area"]['h'], reverse=True))
+
     facial_area = result[0]["facial_area"]
     x = facial_area['x']
     y = facial_area['y']
@@ -48,6 +55,10 @@ if __name__ == "__main__":
     model.to(device)
     model.eval()
 
+    prototxtPath = r"face_detector/deploy.prototxt"
+    weightsPath = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+    faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
     cap = cv2.VideoCapture(0) # webcam id
     while True:
         suc, frame = cap.read()
@@ -59,45 +70,69 @@ if __name__ == "__main__":
         if key == 27:
             break
         elif key == ord('b') or key == ord('B'): # press b to capture face
-            face1 = frame.copy()
-            face1 = get_face(face1)
-            cv2.imwrite(os.path.join(save_demo, "query.jpg"), face1)
-            pre1 = preprocess(face1)
-            ims = torch.from_numpy(pre1).to(device)
-            with torch.no_grad():
-                feat = model(ims)
-            feat = feat.cpu().numpy() # query im feature
-
-            database = "database"
-            names = os.listdir(database)
-
-            most_sim = None
-            most_sim_conf = -1
-            idx = None
-
-            for name in names:
-                dfeat = np.load(os.path.join(database, name, "feat.npy"))
-                cosine = (feat @ dfeat.T) / (norm(feat, axis=-1) * norm(dfeat, axis=-1))
-
-                indexes = np.argsort(cosine)[0, -5:] # get maxmimum most k similar images
-                cosine = cosine[0, indexes]
-                print(cosine, cosine.mean(), indexes)
-
-                conf = (cosine.mean() + 1) / 2
-                # conf = np.arccos(cosine.mean()) / np.pi
-
-                if conf > 0.8 and conf > most_sim_conf:
-                    most_sim = name
-                    most_sim_conf = conf
-                    idx = indexes
-            
-            print(most_sim, most_sim_conf)
-            if most_sim is not None:
-                cv2.putText(frame, f"{most_sim} - {most_sim_conf}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+            # face detection
+            (h, w) = frame.shape[:2]
+            blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                (104.0, 177.0, 123.0))
+            faceNet.setInput(blob) # pass the blob through the network and obtain the face detections
+            detections = faceNet.forward()
+            locs = []
+            # loop over the detections
+            for i in range(0, detections.shape[2]):
+                confidence = detections[0, 0, i, 2]
+                if confidence > 0.5:
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+                    (startX, startY) = (max(0, startX), max(0, startY))
+                    (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+                    locs.append((startX, startY, endX, endY))
+                
+            if not len(locs):
+                cv2.putText(frame, f"No Face in image", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
                     1, (0,255,0), 2, cv2.LINE_AA)
+                idx=None
             else:
-                cv2.putText(frame, f"Stranger", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                    1, (0,255,0), 2, cv2.LINE_AA)
+                locs_max = max(locs, key=lambda x: (x[2]-x[0]) * ([x[3]-x[1]]))
+                (startX, startY, endX, endY) = locs_max
+                face1 = frame[startY:endY, startX:endX]
+
+                cv2.imwrite(os.path.join(save_demo, "query.jpg"), face1)
+                pre1 = preprocess(face1)
+                ims = torch.from_numpy(pre1).to(device)
+                with torch.no_grad():
+                    feat = model(ims)
+                feat = feat.cpu().numpy() # query im feature
+
+                database = "database"
+                names = os.listdir(database)
+
+                most_sim = None
+                most_sim_conf = -1
+                idx = None
+
+                for name in names:
+                    dfeat = np.load(os.path.join(database, name, "feat.npy"))
+                    cosine = (feat @ dfeat.T) / (norm(feat, axis=-1) * norm(dfeat, axis=-1))
+
+                    indexes = np.argsort(cosine)[0, -5:] # get maxmimum most k similar images
+                    cosine = cosine[0, indexes]
+                    print(cosine, cosine.mean(), indexes)
+
+                    conf = (cosine.mean() + 1) / 2
+                    # conf = np.arccos(cosine.mean()) / np.pi
+
+                    if conf > 0.8 and conf > most_sim_conf:
+                        most_sim = name
+                        most_sim_conf = conf
+                        idx = indexes
+                
+                print(most_sim, most_sim_conf)
+                if most_sim is not None:
+                    cv2.putText(frame, f"{most_sim} - {most_sim_conf}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                        1, (0,255,0), 2, cv2.LINE_AA)
+                else:
+                    cv2.putText(frame, f"Stranger", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                        1, (0,255,0), 2, cv2.LINE_AA)
             
             cv2.imshow("frame", frame)
             cv2.waitKey(0)
@@ -119,11 +154,3 @@ if __name__ == "__main__":
                 
         
         cv2.imshow("frame", frame)
-        
-    
-
-
-
-
-
-
